a610b2e6c7a8dab48ffbba6696cbefca3b77ac66
Arsen Grigoryan <happysmilik@gmail.com>
2021-11-08 10:16:41 +0300

    add tests

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -9,24 +9,9 @@ class BonusServiceTest {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1000_60;
+        long amount = 1_000_60;
         boolean registered = true;
         long expected = 30;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void shouldCalculateWhenRegisteredAndBonusOverLimit() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 1_000_000_60;
-        boolean registered = true;
-        long expected = 500;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -50,6 +35,21 @@ class BonusServiceTest {
     }
 
     @Test
+    void shouldCalculateWhenRegisteredAndBonusOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000_60;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void shouldCalculateWhenUnregisteredAndBonusOverLimit() {
         BonusService service = new BonusService();
 
@@ -57,6 +57,66 @@ class BonusServiceTest {
         long amount = 1_000_000_60;
         boolean registered = false;
         long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateWhenRegisteredAndZeroAmount() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 0;
+        boolean registered = true;
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateWhenUnregisteredAndZeroAmount() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 0;
+        boolean registered = false;
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateWhenRegisteredAndNegativeAmount() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = -1_000_60;
+        boolean registered = true;
+        long expected = -30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateWhenUnregisteredAndNegativeAmount() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = -1_000_60;
+        boolean registered = false;
+        long expected = -10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
